c47bc5cc69f0f3e18ffe8492688e1eae968a5f85
Maximilian König <makoeta@gmx.com>
2024-04-19 16:41:41 +0200

    added: tests for new functions and class

--- backend/src/tests/models/game.test.ts
+++ backend/src/tests/models/game.test.ts
@@ -1,13 +1,13 @@
 import {beforeEach, expect, test} from "bun:test";
 import {Game} from "../../models/game";
-import {ScoreChange, Team} from "../../models/team";
+import {ScoreChange} from "../../models/team";
 
 
 let gameObj = new Game('gameId');
 
 
 beforeEach(() => {
-    gameObj = new Game('gameId');
+  gameObj = new Game('gameId');
 })
 
 
@@ -16,31 +16,79 @@ beforeEach(() => {
  */
 
 test('home team scores once', () => {
-    gameObj.homeTeam.updateScore(ScoreChange.INCREASE);
+  gameObj.homeTeam.updateScore(ScoreChange.INCREASE);
 
-    expect(gameObj.homeTeam.score).toBe(1);
+  expect(gameObj.homeTeam.score).toBe(1);
 });
 
 test('guest team scores once', () => {
-    gameObj.guestTeam.updateScore(ScoreChange.INCREASE);
+  gameObj.guestTeam.updateScore(ScoreChange.INCREASE);
 
-    expect(gameObj.guestTeam.score).toBe(1);
+  expect(gameObj.guestTeam.score).toEqual(1);
 });
 
 test('decrease of a score', () => {
-    gameObj.homeTeam.updateScore(ScoreChange.INCREASE);
-    gameObj.homeTeam.updateScore(ScoreChange.INCREASE);
+  gameObj.homeTeam.updateScore(ScoreChange.INCREASE);
+  gameObj.homeTeam.updateScore(ScoreChange.INCREASE);
 
-    gameObj.homeTeam.updateScore(ScoreChange.DECREASE);
+  gameObj.homeTeam.updateScore(ScoreChange.DECREASE);
 
-    expect(gameObj.homeTeam.score).toEqual(1)
+  expect(gameObj.homeTeam.score).toEqual(1)
+})
+
+test('white team wins', () => {
+  for (let i = 0; i < 10; i++) {
+    gameObj.homeTeam.updateScore(ScoreChange.INCREASE);
+  }
+
+  expect(gameObj.winnerTeam).toEqual(gameObj.homeTeam)
 })
 
 
-test('white team wins', () => {
-    for (let i = 0; i < 10; i++) {
-        gameObj.homeTeam.updateScore(ScoreChange.INCREASE);
-    }
+test('winning team does not change', () => {
+  for (let i = 0; i < 10; i++) {
+    gameObj.homeTeam.updateScore(ScoreChange.INCREASE);
+  }
 
-    expect(gameObj.winnerTeam).toEqual(gameObj.homeTeam)
+  for (let i = 0; i < 10; i++) {
+    gameObj.guestTeam.updateScore(ScoreChange.INCREASE);
+  }
+
+  expect(gameObj.winnerTeam).toEqual(gameObj.homeTeam)
+})
+
+test('winning team is reset after last goal got undo', () => {
+  for (let i = 0; i < 10; i++) {
+    gameObj.homeTeam.updateScore(ScoreChange.INCREASE);
+  }
+
+  gameObj.homeTeam.updateScore(ScoreChange.DECREASE)
+
+
+  expect(gameObj.winnerTeam).toBeUndefined()
+})
+
+test('winning team is reset after last goal got undo', () => {
+  for (let i = 0; i < 10; i++) {
+    gameObj.homeTeam.updateScore(ScoreChange.INCREASE);
+  }
+
+  gameObj.homeTeam.updateScore(ScoreChange.DECREASE)
+
+  expect(gameObj.winnerTeam).toBeUndefined()
+})
+
+test('undo winning team and other team wins', () => {
+  for (let i = 0; i < 10; i++) {
+    gameObj.homeTeam.updateScore(ScoreChange.INCREASE);
+  }
+
+  for (let i = 0; i < 9; i++) {
+    gameObj.guestTeam.updateScore(ScoreChange.INCREASE);
+  }
+
+  gameObj.homeTeam.updateScore(ScoreChange.DECREASE)
+  gameObj.guestTeam.updateScore(ScoreChange.INCREASE)
+
+  expect(gameObj.winnerTeam).toEqual(gameObj.guestTeam)
 })
