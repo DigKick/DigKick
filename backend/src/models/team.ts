a11f7139529665ab399a45ec4222cbdfe5a5016c
Maximilian König <makoeta@gmx.com>
2024-04-19 16:02:16 +0200

    change: Team is now a class with attributes of color and score

--- backend/src/models/team.ts
+++ backend/src/models/team.ts
@@ -1,4 +1,25 @@
-export enum Team {
-  WHITE = "WHITE",
-  BLACK = "BLACK"
+export enum TeamColor {
+  WHITE = "white",
+  BLACK = "black"
+}
+
+export enum ScoreChange {
+  DECREASE = -1,
+  INCREASE = 1
+}
+
+export class Team {
+
+  public color: TeamColor
+  public score: number
+
+
+  constructor(color: TeamColor) {
+    this.color = color;
+    this.score = 0;
+  }
+
+  changeScore(change: ScoreChange) {
+    this.score = Math.max(0, this.score + change)
+  }
 }
