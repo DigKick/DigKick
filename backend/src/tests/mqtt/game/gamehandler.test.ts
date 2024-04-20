b278a5a86a42c24d4aa586e150858a32e521e872
Maximilian König <makoeta@gmx.com>
2024-04-20 14:59:24 +0200

    added: new tests

--- backend/src/tests/mqtt/game/gamehandler.test.ts
+++ backend/src/tests/mqtt/game/gamehandler.test.ts
@@ -6,11 +6,19 @@ import {GameEventType} from "../../../mqtt/game/events/gameevents";
 
 let gameObj = new Game('gameId');
 let gameHandler = new GameHandler(gameObj)
+let emptyMockFunc = mock()
 
-const emptyMockFunc = mock()
+let cbGame!: Game | undefined
+const gameObs = (game: Game) => {
+  cbGame = game
+}
+
+
 beforeEach(() => {
   gameObj = new Game('gameId');
   gameHandler = new GameHandler(gameObj)
+  emptyMockFunc = mock()
+  cbGame = undefined
 })
 
 
@@ -28,13 +36,45 @@ test('remove observer', () => {
 })
 
 test('test callback', () => {
-  let cbGame!: Game
+  gameHandler.subscribe(emptyMockFunc)
+  gameHandler.triggerEvent(GameEventType.HOME_SCORE_INCREASE)
+  expect(emptyMockFunc.mock.calls.length).toEqual(1)
+})
 
-  gameHandler.subscribe((game: Game) => {
-    cbGame = game
-  })
-
+test('test event home increase', () => {
+  gameHandler.subscribe(gameObs)
   gameHandler.triggerEvent(GameEventType.HOME_SCORE_INCREASE)
 
-  expect(cbGame.homeTeam.score).toEqual(1)
+  expect(cbGame!.homeTeam.score).toEqual(1)
+})
+
+
+test('test event home decrease', () => {
+  gameHandler.subscribe(gameObs)
+  gameHandler.triggerEvent(GameEventType.HOME_SCORE_INCREASE)
+  gameHandler.triggerEvent(GameEventType.HOME_SCORE_DECREASE)
+  expect(cbGame!.homeTeam.score).toEqual(0)
+})
+
+
+test('test event guest increase', () => {
+  gameHandler.subscribe(gameObs)
+  gameHandler.triggerEvent(GameEventType.GUEST_SCORE_INCREASE)
+
+  expect(cbGame!.guestTeam.score).toEqual(1)
+})
+
+
+test('test event guest decrease', () => {
+  gameHandler.subscribe(gameObs)
+  gameHandler.triggerEvent(GameEventType.GUEST_SCORE_INCREASE)
+  gameHandler.triggerEvent(GameEventType.GUEST_SCORE_DECREASE)
+
+  expect(cbGame!.guestTeam.score).toEqual(0)
+})
+
+test('test invalid event type', () => {
+  expect(() => {
+    gameHandler.triggerEvent(GameEventType._INCREASE)
+  }).toThrowError(Error)
 })
