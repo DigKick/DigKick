7252dcca9f5c8b2e41371338b69baf25058375a7
Maximilian König <makoeta@gmx.com>
2024-04-20 21:37:06 +0200

    changed: callbacks now subscribe to an event

--- backend/src/mqtt/game/handler/gamehandler.ts
+++ backend/src/mqtt/game/handler/gamehandler.ts
@@ -3,26 +3,47 @@ import {gameEventMapper, GameEventType} from "../events/gameevents";
 
 export class GameHandler {
 
-  public observers: Function[] = []
-  public game: Game
+  public observerMap: Map<GameEventType, Function> = new Map();
+  public game!: Game
 
-  constructor(game: Game) {
-    this.game = game;
+  constructor() {
+    this.newGame()
+  }
+
+
+  public newGame() {
+    this.game = new Game('')
+    this._notifyObserver(GameEventType.START)
   }
 
   public triggerEvent(event: GameEventType) {
-    gameEventMapper(event, this.game)
-    this.observers.forEach((obs) => obs(this.game))
+    const triggeredEvents = gameEventMapper(event, this.game)
+    triggeredEvents.forEach((event) => {
+      this._notifyObserver(event);
+    })
   }
 
-  public subscribe(observer: Function) {
-    if (this.observers.includes(observer)) {
+  private _notifyObserver(event: GameEventType) {
+    const callback = this.observerMap.get(event)
+    if (callback) {
+      callback(this.game)
+    }
+  }
+
+  public subscribe(event: GameEventType, observer: Function) {
+    if (Array.from(this.observerMap.values()).includes(observer)) {
+      // observer already subscribed
       return;
     }
-    this.observers.push(observer)
+
+    this.observerMap.set(event, observer)
   }
 
   public unsubscribe(observer: Function) {
-    this.observers = this.observers.filter((obs) => obs !== observer);
+    Array.from(this.observerMap.entries()).forEach((entry) => {
+      if (entry[1] === observer) {
+        this.observerMap.delete(entry[0])
+      }
+    })
   }
 }
