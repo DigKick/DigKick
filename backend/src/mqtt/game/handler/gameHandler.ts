6333745f3e5ed1390abaf1706c811a86264b03bd
Maximilian König <makoeta@gmx.com>
2024-04-23 20:00:26 +0200

    redo: weird changes

--- backend/src/mqtt/game/handler/gameHandler.ts
+++ backend/src/mqtt/game/handler/gameHandler.ts
@@ -1,20 +1,17 @@
-import {Game} from "../../../models/game";
 import {gameEventMapper, GameEventType} from "../events/gameEvent";
 import {SoccerTable} from "../../../models/soccerTable";
 
 export class GameHandler {
 
   public observerMap: Map<GameEventType, Function> = new Map();
-  public game!: Game
   public soccerTable: SoccerTable
 
   constructor(soccerTable: SoccerTable) {
     this.soccerTable = soccerTable;
-    this.game = soccerTable.game;
   }
 
   public triggerEvent(event: GameEventType) {
-    const triggeredEvents = gameEventMapper(event, this.game)
+    const triggeredEvents = gameEventMapper(event, this.soccerTable.game)
     triggeredEvents.forEach((event) => {
       this._notifyObserver(event);
     })
@@ -23,7 +20,7 @@ export class GameHandler {
   private _notifyObserver(event: GameEventType) {
     const callback = this.observerMap.get(event)
     if (callback) {
-      callback(this.game)
+      callback(this.soccerTable.game)
     }
   }
 
