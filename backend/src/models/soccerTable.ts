9d8c5d9733c5b6e2e20316a2c008b54589a93609
Maximilian König <makoeta@gmx.com>
2024-04-23 18:16:40 +0200

    change: SoccerTable now holds game object

--- backend/src/models/soccerTable.ts
+++ backend/src/models/soccerTable.ts
@@ -1,8 +1,20 @@
+import {Game} from "./game";
+
 export class SoccerTable {
 
   public id: string
+  private _game: Game;
 
   constructor(id: string) {
     this.id = id;
+    this._game = new Game();
+  }
+
+  get game() {
+    return this._game;
+  }
+
+  newGame() {
+    this._game = new Game();
   }
 }
