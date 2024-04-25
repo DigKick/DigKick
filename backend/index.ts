3079e8a05a55c2ab4953f8938865899f3e8b6918
Maximilian König <maximilian.koenig@mein.gmx>
2024-04-25 20:11:58 +0200

    added: index now instantiate a soccer table register handler

--- backend/index.ts
+++ backend/index.ts
@@ -1,3 +1,5 @@
-import { DkMqttClient } from "./src/mqtt/client";
+import {DkMqttClient} from "./src/mqtt/client/client";
+import {SoccerTableRegisterHandler} from "./src/mqtt/soccerTable/register/handler/soccerTableRegisterHandler";
 
 DkMqttClient.getInstance();
+new SoccerTableRegisterHandler();
