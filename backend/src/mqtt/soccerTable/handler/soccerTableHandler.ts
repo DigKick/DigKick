2a1f25300d5a24b3f9732a02797bce48ed8ec443
Maximilian König <maximilian.koenig@mein.gmx>
2024-04-26 21:31:08 +0200

    rename

--- backend/src/mqtt/soccerTable/handler/soccerTableHandler.ts
+++ backend/src/mqtt/soccerTable/handler/soccerTableHandler.ts
@@ -1,11 +1,16 @@
-import {SoccerTable} from "../../../models/soccerTable";
-import {GameHandler} from "../../game/handler/gameHandler";
-import {SoccerTableEvent, soccerTableEventMapper} from "../events/soccerTableEvent";
-import {AbstractHandler} from "../../abstract/AbstractHandler";
+import { SoccerTable } from "../../../models/soccerTable";
+import { GameHandler } from "../../game/handler/gameHandler";
+import {
+  SoccerTableEvent,
+  soccerTableEventMapper,
+} from "../events/soccerTableEvent";
+import { AbstractHandler } from "../../abstract/abstractHandler";
 
-export class SoccerTableHandler extends AbstractHandler<SoccerTableEvent, SoccerTable> {
-
-  private readonly _gameHandler: GameHandler
+export class SoccerTableHandler extends AbstractHandler<
+  SoccerTableEvent,
+  SoccerTable
+> {
+  private readonly _gameHandler: GameHandler;
 
   constructor(soccerTable: SoccerTable) {
     super(soccerTable, soccerTableEventMapper);
