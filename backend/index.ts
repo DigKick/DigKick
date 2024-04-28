13a7f5bcbfc2aa76cf354eb976e06844b2a137d2
Maximilian König <makoeta@gmx.de>
2024-04-28 03:23:54 +0200

    added: logo at boot

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,7 @@
-import { DkMqttClient } from "./src/mqtt/client/client";
-import { SoccerTableRegisterHandler } from "./src/mqtt/soccerTable/handler/soccerTableRegisterHandler";
+import {DkMqttClient} from "./src/mqtt/client/client";
+import {SoccerTableRegisterHandler} from "./src/mqtt/soccerTable/handler/soccerTableRegisterHandler";
+import {LoggerFactory} from "./src/logging/loggerFactory";
 
+LoggerFactory.printLogo();
 DkMqttClient.getInstance();
 new SoccerTableRegisterHandler();
