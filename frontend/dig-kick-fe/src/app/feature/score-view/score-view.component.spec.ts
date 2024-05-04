7f9847e8ef4007e09d3e5b2425d48f6e100c5d46
Marvin Voss <voss.marvin@gmail.com>
2024-05-04 21:57:45 +0200

    added score-view component

---
+++ frontend/dig-kick-fe/src/app/feature/score-view/score-view.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ScoreViewComponent } from './score-view.component';
+
+describe('ScoreViewComponent', () => {
+  let component: ScoreViewComponent;
+  let fixture: ComponentFixture<ScoreViewComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ScoreViewComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(ScoreViewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/dig-kick-fe/src/app/feature/score-view/score-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-score-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './score-view.component.html',
+  styleUrl: './score-view.component.css'
+})
+export class ScoreViewComponent {
+
+}
