be9f59ace8689a76054a165924f25486f6d5af37
Marvin Voss <voss.marvin@gmail.com>
2024-05-04 22:31:42 +0200

    added routing

--- frontend/dig-kick-fe/src/app/app.routes.ts
+++ frontend/dig-kick-fe/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Route } from '@angular/router';
+import { GameViewComponent } from './feature/game-view/game-view.component';
+import { ScoreViewComponent } from './feature/score-view/score-view.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {path: '', component: GameViewComponent},
+    {path: 'score', component: ScoreViewComponent}
+];
