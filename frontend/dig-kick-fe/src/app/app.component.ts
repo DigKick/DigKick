2483ba05cbabfbd43049e09e1dbdbcb128e76d16
Marvin Voss <voss.marvin@gmail.com>
2024-05-04 21:47:56 +0200

    added game-view component

--- frontend/dig-kick-fe/src/app/app.component.ts
+++ frontend/dig-kick-fe/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NavbarComponent } from './core/static/components/navbar/navbar.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, NavbarComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
---
+++ frontend/dig-kick-fe/src/app/feature/game-view/game-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './game-view.component.html',
+  styleUrl: './game-view.component.css'
+})
+export class GameViewComponent {
+
+}
