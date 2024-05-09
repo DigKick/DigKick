7f2f36f1e019f50c6143a64ecb7d7b176e64f915
Marvin Voss <voss.marvin@gmail.com>
2024-05-09 03:11:18 +0200

    reworked game view

--- frontend/dig-kick-fe/src/app/feature/game-view/game-view.component.ts
+++ frontend/dig-kick-fe/src/app/feature/game-view/game-view.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Player } from 'src/app/core/static/models/player.model';
 
 @Component({
   selector: 'app-game-view',
@@ -8,5 +9,37 @@ import { Component } from '@angular/core';
   styleUrl: './game-view.component.css'
 })
 export class GameViewComponent {
+  player1: Player;
+  player2: Player;
+  player3: Player;
+  player4: Player;
 
+  constructor() {
+    this.player1 = {
+      id: String(1),
+      firstname: 'player',
+      lastname: '1',
+      score: 345
+    }
+   this.player2 = {
+      id: String(2),
+      firstname: 'player',
+      lastname: '2',
+      score: 217
+    }
+    this.player3 = {
+      id: String(3),
+      firstname: 'player',
+      lastname: '3',
+      score: 313
+    }
+    this.player4 = {
+      id: String(4),
+      firstname: 'player',
+      lastname: '4',
+      score: 123
+    }
+  }
+
+  
 }
