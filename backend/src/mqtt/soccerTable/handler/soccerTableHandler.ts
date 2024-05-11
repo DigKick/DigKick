808d68f715448521774e7112dd57b215d2338073
Maximilian König <makoeta@gmx.com>
2024-05-11 18:59:57 +0200

    changed: soccertablehandler now instantiates two hardware handlers

--- backend/src/mqtt/soccerTable/handler/soccerTableHandler.ts
+++ backend/src/mqtt/soccerTable/handler/soccerTableHandler.ts
@@ -2,15 +2,22 @@ import {SoccerTable} from "../../../models/soccerTable";
 import {GameHandler} from "../../game/handler/gameHandler";
 import {SoccerTableEvent, soccerTableEventMapper,} from "../events/soccerTableEvent";
 import {AbstractHandler, HandlerType} from "../../abstract/abstractHandler";
+import {HardwareHandler} from "../../hardware/handler/hardwareHandler";
+import {HardwareTopicManager} from "../../hardware/topics/hardwareTopicManager";
+import {TeamColor} from "../../../models/team";
 
 export class SoccerTableHandler extends AbstractHandler<
   SoccerTableEvent,
   SoccerTable
 > {
   private readonly _gameHandler: GameHandler;
+  private readonly _hardwareHandlerWhite: HardwareHandler;
+  private readonly _hardwareHandlerBlack: HardwareHandler;
 
   constructor(soccerTable: SoccerTable) {
     super(soccerTable, soccerTableEventMapper, HandlerType.SOCCERTABLE, soccerTable);
+    this._hardwareHandlerWhite = new HardwareHandler(this, new HardwareTopicManager(this.subject.id, TeamColor.WHITE))
+    this._hardwareHandlerBlack = new HardwareHandler(this, new HardwareTopicManager(this.subject.id, TeamColor.BLACK))
     this._gameHandler = new GameHandler(this.subject);
   }
 
