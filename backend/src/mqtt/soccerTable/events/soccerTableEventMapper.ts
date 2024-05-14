6a59beb98090ca2c72c784284df245996eb94a49
Maximilian König <makoeta@gmx.de>
2024-05-14 02:14:38 +0200

    added: SoccerTable event mappings

--- backend/src/mqtt/soccerTable/events/soccerTableEventMapper.ts
+++ backend/src/mqtt/soccerTable/events/soccerTableEventMapper.ts
@@ -1,32 +1,53 @@
 import type {EventMapper} from "../../abstract/eventMapper";
 import {SoccerTableEventType} from "./soccerTableEventType";
 import {SoccerTable} from "../../../models/soccerTable";
+import {GameHandler} from "../../game/handler/gameHandler";
+import {GameEventType} from "../../game/events/gameEvent";
+import {DkMqttClient} from "../../client/client";
+import {BaseTopicFactory} from "../../util/baseTopicFactory";
+import {SoccerTablePayload} from "../payloads/SoccerTablePayload";
+
 
 export class SoccerTableEventMapper implements EventMapper<SoccerTableEventType>{
 
-  private _soccerTable: SoccerTable;
+  private _gameHandler: GameHandler;
+  private readonly _soccerTable: SoccerTable;
 
-
-  constructor(soccerTable: SoccerTable) {
+  constructor(soccerTable: SoccerTable, gameHandler: GameHandler) {
     this._soccerTable = soccerTable;
+    this._gameHandler = gameHandler;
   }
 
   map(event: SoccerTableEventType) {
     let triggeredEvents = [event]
+    const dkMqttClient: DkMqttClient = DkMqttClient.getInstance()
 
     switch (event) {
       case SoccerTableEventType.NEW_GAME:
         this._soccerTable.newGame();
+        this._gameHandler.triggerEvent(GameEventType.WHITE_SCORE_CHANGE)
+        this._gameHandler.triggerEvent(GameEventType.BLACK_SCORE_CHANGE)
+        dkMqttClient.publishWithRetain(BaseTopicFactory.getBaseTopic(this._soccerTable), JSON.stringify(new SoccerTablePayload(event)))
         break;
+
       case SoccerTableEventType.FINISH_GAME:
         this._soccerTable.newGame();
-        // SAVE GAME TO DATABASE
+        this._gameHandler.triggerEvent(GameEventType.WHITE_SCORE_CHANGE)
+        this._gameHandler.triggerEvent(GameEventType.BLACK_SCORE_CHANGE)
+        dkMqttClient.publishWithRetain(BaseTopicFactory.getBaseTopic(this._soccerTable), JSON.stringify(new SoccerTablePayload(event)))
         break;
+
       case SoccerTableEventType.CANCEL_GAME:
         this._soccerTable.newGame();
+        this._gameHandler.triggerEvent(GameEventType.WHITE_SCORE_CHANGE)
+        this._gameHandler.triggerEvent(GameEventType.BLACK_SCORE_CHANGE)
+        dkMqttClient.publishWithRetain(BaseTopicFactory.getBaseTopic(this._soccerTable), JSON.stringify(new SoccerTablePayload(event)))
         break;
+
       default:
+        console.log("SHOULD NOT BE HERE :C")
         break;
+
     }
 
     if (event.includes(SoccerTableEventType.GAME)) {
