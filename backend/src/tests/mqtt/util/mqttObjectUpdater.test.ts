0a8b1dbbf4c22ccab9a0cf39996e46d29dfd9094
Maximilian König <makoeta@gmx.de>
2024-05-19 02:07:56 +0200

    added: working tests

--- backend/src/tests/mqtt/util/mqttObjectUpdater.test.ts
+++ backend/src/tests/mqtt/util/mqttObjectUpdater.test.ts
@@ -1,4 +1,4 @@
-import {test} from "bun:test";
+import {expect, test} from "bun:test";
 import {MqttObjectUpdater} from "../../../mqtt/util/mqttObjectUpdater/mqttObjectUpdater";
 import {SoccerTable} from "../../../models/soccerTable";
 import {BasicTerm} from "../../../mqtt/util/basicTerm";
@@ -8,15 +8,10 @@ test('compare function table', () => {
   let table = new SoccerTable("test")
   let updater = new MqttObjectUpdater<SoccerTable>(table, {prefix: `/${BasicTerm.TABLE}`, instantPublish: true, publishWithRetain: true} )
 
-  table.game.teamBlack.score = 10;
-
   table.game.teamWhite.score = 5;
 
   updater.commit(table)
-  console.log("len: ", Array.from(updater.latestChanges.entries()).length)
-  Array.from(updater.latestChanges.entries()).forEach(entry => {
-    console.log(`Changed: ${entry[0]}: ${entry[1]}`);
-  })
+  expect(updater.latestChanges.size).toEqual(3) // update: game itself, teamWhite, teamWhiteScore
   updater.publish()
 })
 
@@ -24,7 +19,5 @@ test('compare init table', () => {
   let table = new SoccerTable("test")
   let updater = new MqttObjectUpdater<SoccerTable>(table, {prefix: `/${BasicTerm.TABLE}`, instantPublish: true, publishWithRetain: true} )
 
-  Array.from(updater.latestChanges.entries()).forEach(entry => {
-    console.log(`Changed: ${entry[0]}: ${entry[1]}`);
-  })
+  expect(updater.latestChanges.size).toEqual(0)
 })
