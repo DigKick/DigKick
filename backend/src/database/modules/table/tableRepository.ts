de60ead30e50646d3a76b890d276fa5e6b8b94a4
Maximilian König <makoeta@gmx.com>
2024-06-06 11:33:14 +0200

    added: tableRepository.ts

--- backend/src/database/modules/table/tableRepository.ts
+++ backend/src/database/modules/table/tableRepository.ts
@@ -1,8 +1,12 @@
 import type {Table} from "../../../models/table.ts";
 import {TableEntity} from "./tableEntity.ts";
 import {TableParser} from "./tableParser.ts";
+import {Logger} from "winston";
+import {LoggerFactory} from "../../../logging/loggerFactory.ts";
+import {GameRepository} from "../game/gameRepository.ts";
 
 export class TableRepository {
+  private static logger: Logger = LoggerFactory.getLogger(GameRepository.name);
 
   public static async saveTable(table: Table) {
     try {
@@ -10,7 +14,7 @@ export class TableRepository {
 
       await TableEntity.save(tableEntity)
     } catch (e) {
-
+      this.logger.error(`Could not save or parse table: ${e}`)
     }
   }
 
