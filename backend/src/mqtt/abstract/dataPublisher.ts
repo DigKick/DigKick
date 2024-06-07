7e7e0c2a869efe006d8bf31c7c56f885b6f8d175
Maximilian König <makoeta@gmx.com>
2024-06-07 19:41:57 +0200

    added: abstract DataPublisher

---
+++ backend/src/mqtt/abstract/dataPublisher.ts
@@ -0,0 +1,14 @@
+export abstract class DataPublisher {
+
+  protected static recentItemCount = 10;
+
+  public static publishRecent() {
+  }
+
+  public static publishById(id: number) {
+  }
+
+  public static publishRequested() {
+  }
+
+}
