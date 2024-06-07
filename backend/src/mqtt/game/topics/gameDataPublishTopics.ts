b253f6266677308bc6b18761f3b0cd97675efc03
Maximilian König <makoeta@gmx.com>
2024-06-07 18:24:54 +0200

    added: enum for game data publishing

---
+++ backend/src/mqtt/game/topics/gameDataPublishTopics.ts
@@ -0,0 +1,9 @@
+import {DataPublishTopic} from "../../abstract/dataPublishTopic.ts";
+import {BasicTerm} from "../../util/basicTerm.ts";
+
+export enum GameDataPublishTopics {
+  BASE = `/${DataPublishTopic.BASE}/${BasicTerm.GAME}`,
+  RECENT = `${GameDataPublishTopics.BASE}/${DataPublishTopic.RECENT}`,
+  ID = `${GameDataPublishTopics.BASE}/${DataPublishTopic.ID}`,
+  REQUESTED = `${GameDataPublishTopics.BASE}/${DataPublishTopic.REQUESTED}`,
+}
