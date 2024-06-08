35562364d6e50642b4f9fdb4dba0012e25a6d45c
Maximilian König <makoeta@gmx.com>
2024-06-08 02:27:37 +0200

    added: handling requests

--- backend/src/mqtt/global/publishing/dataRequestHandler.ts
+++ backend/src/mqtt/global/publishing/dataRequestHandler.ts
@@ -3,7 +3,15 @@ import {DataPublishTopic} from "./dataPublishTopic.ts";
 import {LoggerFactory} from "../../../logging/loggerFactory.ts";
 import {BasicTerm} from "../../util/basicTerm.ts";
 import type {TopicSubscriber} from "../../client/topicSubscriber.ts";
-import type {RequestPayload} from "./payloads/requestPayload.ts";
+import {
+  type RequestPayload,
+  type RequestPayloadById,
+  type RequestPayloadByRecent,
+  type RequestPayloadByTimeSpan,
+  RequestPayloadType
+} from "./payloads/requestPayload.ts";
+import {RequestPayloadParser} from "./payloads/requestPayloadParser.ts";
+import {TopicTool} from "../../util/topicTool.ts";
 
 export class DataRequestHandler {
   private static _logger = LoggerFactory.getLogger(DataRequestHandler.name)
@@ -13,9 +21,7 @@ export class DataRequestHandler {
   private static dataPublisher: Map<BasicTerm, DataPublisher> = new Map();
   private static requestSubscriber: TopicSubscriber = {
     topic: DataPublishTopic.REQUEST_TOPIC.toString(),
-    func: (topic: string, payload: RequestPayload) => {
-
-    }
+    func: (topic: string, payload: RequestPayload) => this.handleRequest(topic, payload)
   }
 
   public static getInstance(): DataRequestHandler {
@@ -28,7 +34,7 @@ export class DataRequestHandler {
   private constructor() {
   }
 
-  public static registerDataPublisher(type: BasicTerm, publisher: DataPublishTopic) {
+  public static registerDataPublisher(type: BasicTerm, publisher: DataPublisher) {
     if (this.dataPublisher.get(type)) {
       this._logger.warn(`DataPublisher for '${type}' already registered.`);
       return
@@ -37,9 +43,46 @@ export class DataRequestHandler {
     this._logger.debug(`DataPublisher for '${type}' registered.`)
   }
 
-  private static handleRequest() {
+  private static handleRequest(topic: string, payload: RequestPayload) {
+    let parsedPayload;
 
+    try {
+      parsedPayload = RequestPayloadParser.parseRequestPayload(payload);
+    } catch (error) {
+      this._logger.warn(`Could not parse payload: ${payload}`)
+      return
+    }
+
+    const topicTool = new TopicTool(topic);
+    let matchingPublisher: DataPublisher | undefined = this.dataPublisher.get(topicTool.getSegment(1) as BasicTerm)
+
+    if (!matchingPublisher) {
+      this._logger.warn(`No publisher for type ${topicTool.getSegment(1)} does not exist.`);
+      return;
+    }
+
+    DataRequestHandler._publishRequestPayload(parsedPayload, matchingPublisher);
   }
 
+  private static _publishRequestPayload(parsedPayload: any, matchingPublisher: DataPublisher) {
+    switch (parsedPayload.type) {
+      case RequestPayloadType.BY_ID:
+        parsedPayload = parsedPayload as RequestPayloadById
+        matchingPublisher.publishById(parsedPayload.id)
+        break
 
+      case RequestPayloadType.BY_RECENT:
+        parsedPayload = parsedPayload as RequestPayloadByRecent
+        matchingPublisher.publishRecent(parsedPayload.amount)
+        break
+
+      case RequestPayloadType.BY_TIME:
+        parsedPayload = parsedPayload as RequestPayloadByTimeSpan
+        matchingPublisher.publishByTimeSpan(parsedPayload.from, parsedPayload.to)
+        break
+
+      default:
+        this._logger.error("Implementation error.")
+    }
+  }
 }
