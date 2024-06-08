b4baeef1c5c05dede2aa024628cdd05edb2bfa17
Maximilian König <makoeta@gmx.com>
2024-06-08 03:14:40 +0200

    removed: unused topic

--- backend/src/mqtt/game/topics/gameDataPublishTopics.ts
+++ backend/src/mqtt/game/topics/gameDataPublishTopics.ts
@@ -4,6 +4,5 @@ import {BasicTerm} from "../../util/basicTerm.ts";
 export enum GameDataPublishTopics {
   BASE = `/${DataPublishTopic.BASE}/${BasicTerm.GAME}`,
   RECENT = `${GameDataPublishTopics.BASE}/${DataPublishTopic.RECENT}`,
-  ID = `${GameDataPublishTopics.BASE}/${DataPublishTopic.ID}`,
   REQUESTED = `${GameDataPublishTopics.BASE}/${DataPublishTopic.REQUEST}`,
 }
