e1472dbb5efe739b9abb131168cb1367cbcc2f3b
Maximilian König <makoeta@gmx.com>
2024-06-11 20:50:42 +0200

    removed: publish get requested method

--- backend/src/mqtt/global/publishing/dataPublisher.ts
+++ backend/src/mqtt/global/publishing/dataPublisher.ts
@@ -1,19 +1,14 @@
-import type {RequestPayload} from "./payloads/requestPayload.ts";
-
 export class DataPublisher {
 
   protected static recentItemCount = 10;
 
-  public publishRecent(amount: number) {
+  public async publishRecent(amount: number) {
   }
 
-  public publishById(id: number) {
+  public async publishById(id: number) {
   }
 
-  public publishRequested(request: RequestPayload) {
-  }
-
-  public publishByTimeSpan(from: Date, to: Date) {
+  public async publishByTimeSpan(from: Date, to: Date) {
   }
 
 }
