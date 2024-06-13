b85d189c5b97d5cf851d1e27b7aaf3edf3b34b70
Maximilian König <makoeta@gmx.com>
2024-06-13 15:58:31 +0200

    adjusted: started map method for nfc reader mapper

--- backend/src/mqtt/hardware/events/nfcReaderEventMapper.ts
+++ backend/src/mqtt/hardware/events/nfcReaderEventMapper.ts
@@ -1,6 +1,8 @@
 import type {EventMapper} from "../../global/eventMapper.ts";
-import {type NfcReaderEventType} from "./nfcReaderEvent.ts";
+import {NfcReaderEventType} from "./nfcReaderEvent.ts";
 import {type PlayerHandler} from "../../player/handler/playerHandler.ts";
+import {PlayerEventType} from "../../player/events/playerEvent.ts";
+import {TopicTool} from "../../util/topicTool.ts";
 
 export class NfcReaderEventMapper implements EventMapper<NfcReaderEventType> {
 
@@ -11,8 +13,19 @@ export class NfcReaderEventMapper implements EventMapper<NfcReaderEventType> {
     this._playerHandler = playerHandler;
   }
 
-  map(event: NfcReaderEventType, topic: string | undefined, payload: any) {
-    return new Set([event]);
+  map(event: NfcReaderEventType, topic: string, payload: any) {
+    const triggeredEvents = new Set([event])
+
+    const topicTool = new TopicTool(topic)
+
+
+    switch (event) {
+      case NfcReaderEventType.LOGIN_PLAYER:
+        this._playerHandler.triggerEvent(PlayerEventType.BLACK_PLAYER_REGISTER_TO_GAME, topic, payload);
+        break
+    }
+
+    return triggeredEvents;
   };
 
 }
