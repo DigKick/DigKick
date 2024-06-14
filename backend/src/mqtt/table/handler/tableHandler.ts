1e016649d917985dc1f32a845dd8605d22cc4676
Maximilian König <makoeta@gmx.de>
2024-06-14 02:23:25 +0200

    added: table handlers instantiates nfc and player handler

--- backend/src/mqtt/table/handler/tableHandler.ts
+++ backend/src/mqtt/table/handler/tableHandler.ts
@@ -5,19 +5,32 @@ import {DkModelHandler, HandlerType} from "../../global/dkModelHandler.ts";
 import {SensorHandler} from "../../hardware/handler/sensorHandler.ts";
 import {TeamColor} from "../../../models/team";
 import {TableEventMapper} from "../events/tableEventMapper.ts";
+import {NfcReaderHandler} from "../../hardware/handler/nfcReaderHandler.ts";
+import {PlayerHandler} from "../../player/handler/playerHandler.ts";
 
 export class TableHandler extends DkModelHandler<
   TableEventType,
   Table
 > {
   private readonly _gameHandler: GameHandler;
-  private readonly _hardwareHandlerWhite: SensorHandler;
-  private readonly _hardwareHandlerBlack: SensorHandler;
+  private readonly _sensorHandlerWhite: SensorHandler;
+  private readonly _sensorHandlerBlack: SensorHandler;
+
+  private readonly _playerHandler: PlayerHandler;
+  private readonly _nfcHandlerWhite: NfcReaderHandler;
+  private readonly _nfcHandlerBlack: NfcReaderHandler;
 
   constructor(soccerTable: Table) {
     super(soccerTable, HandlerType.SOCCERTABLE, soccerTable);
-    this._hardwareHandlerWhite = new SensorHandler(this, TeamColor.WHITE);
-    this._hardwareHandlerBlack = new SensorHandler(this, TeamColor.BLACK);
+
+    this._sensorHandlerWhite = new SensorHandler(this, TeamColor.WHITE);
+    this._sensorHandlerBlack = new SensorHandler(this, TeamColor.BLACK);
+
+    this._nfcHandlerWhite = new NfcReaderHandler(this, TeamColor.WHITE);
+    this._nfcHandlerBlack = new NfcReaderHandler(this, TeamColor.BLACK);
+
+    this._playerHandler = new PlayerHandler(this, this.subject)
+
     this._gameHandler = new GameHandler(this.subject);
     this._mapper = new TableEventMapper(soccerTable, this._gameHandler);
 
@@ -26,5 +39,9 @@ export class TableHandler extends DkModelHandler<
 
   get gameHandler() {
     return this._gameHandler;
+  }
+
+  get playerHandler() {
+    return this._playerHandler;
   }
 }
