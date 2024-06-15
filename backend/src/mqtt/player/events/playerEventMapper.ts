949b1c1a15f1554720b6bbe2ff62c2dceefd07d0
Maximilian König <makoeta@gmx.com>
2024-06-15 17:38:22 +0200

    removed: not needed attributes from the playerEventMapper

--- backend/src/mqtt/player/events/playerEventMapper.ts
+++ backend/src/mqtt/player/events/playerEventMapper.ts
@@ -1,6 +1,5 @@
 import type {EventMapper} from "../../global/eventMapper.ts";
 import {PlayerEventType} from "./playerEvent.ts";
-import {Game} from "../../../models/game.ts";
 import {Table} from "../../../models/table.ts";
 import type {NfcReaderPayload} from "../../hardware/payloads/nfcReaderPayload.ts";
 import {PlayerRepository} from "../../../database/modules/player/playerRepository.ts";
@@ -9,14 +8,9 @@ import {GameEventType} from "../../game/events/gameEvent.ts";
 
 export class PlayerEventMapper implements EventMapper<PlayerEventType> {
 
-  private readonly _game: Game;
-  private readonly _table: Table;
   private readonly _tableHandler: TableHandler;
 
   constructor(tableHandler: TableHandler, table: Table) {
-    this._table = table;
-    this._game = table.game;
-
     this._tableHandler = tableHandler
   }
 
@@ -24,14 +18,14 @@ export class PlayerEventMapper implements EventMapper<PlayerEventType> {
     switch (event) {
       case PlayerEventType.PLAYER_REGISTER_BLACK_TEAM:
         PlayerRepository.getOrCreatePlayer(payload.serialNumber).then((player) => {
-          this._game.teamBlack.addPlayer(player)
+          this._tableHandler.subject.game.teamBlack.addPlayer(player)
           this._tableHandler.gameHandler.triggerEvent(GameEventType.PLAYER_CHANGE, topic, payload)
         })
         break
 
       case PlayerEventType.PLAYER_REGISTER_WHITE_TEAM:
         PlayerRepository.getOrCreatePlayer(payload.serialNumber).then((player) => {
-          this._game.teamWhite.addPlayer(player)
+          this._tableHandler.subject.game.teamWhite.addPlayer(player)
         })
         break
 
