202bb3b3cbf1830b7baa83811fcbd78ee15f0973
Maximilian König <makoeta@gmx.com>
2024-06-15 12:14:18 +0200

    added: player parse tests

---
+++ backend/tests/database/modules/player/playerParser.test.ts
@@ -0,0 +1,29 @@
+import {PlayerEntity} from "../../../../src/database/modules/player/playerEntity.ts";
+import {Player} from "../../../../src/models/player.ts";
+import {expect, test} from "bun:test";
+import {PlayerParser} from "../../../../src/database/modules/player/playerParser.ts";
+
+function createValidPlayerEntity() {
+  const validPlayerEntity = new PlayerEntity()
+
+  validPlayerEntity.name = 'validPlayerEntity'
+
+  return validPlayerEntity
+}
+
+function createValidPlayer() {
+  return new Player("validPlayer", "key", 100);
+}
+
+
+test('valid parse PlayerEntity to Player: name field', () => {
+  const parsedPlayer: Player = PlayerParser.toPlayer(createValidPlayerEntity());
+
+  expect(parsedPlayer.name).not.toBeUndefined()
+})
+
+test('valid parse Player to PlayerEntity: name field', () => {
+  const parsedPlayerEntity: PlayerEntity = PlayerParser.toPlayerEntity(createValidPlayer())
+
+  expect(parsedPlayerEntity.name).not.toBeUndefined()
+})
