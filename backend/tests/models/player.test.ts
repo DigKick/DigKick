6759b5bcf8882b50a5890ed5e249a89fee7e6bae
Maximilian König <makoeta@gmx.de>
2024-06-16 20:44:09 +0200

    added: player consts for testing

---
+++ backend/tests/models/player.test.ts
@@ -0,0 +1,5 @@
+import {Player} from "../../src/models/player.ts";
+
+export const playerOne = new Player("playerOne", "playerOne", 100)
+export const playerTwo = new Player("playerTwo", "playerTwo", 100)
+export const playerThree = new Player("playerThree", "playerThree", 100)
