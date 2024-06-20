b36f8ea02a2741d8ca45c4c7b71b2bdf265b88e2
Marvin <voss.marvin@gmail.com>
2024-06-20 21:35:18 +0200

    removed stats as unnecessary for the moment

--- frontend/dig-kick-fe/src/app/app.component.ts
+++ frontend/dig-kick-fe/src/app/app.component.ts
@@ -6,7 +6,6 @@ import { TableViewComponent } from './feature/table-view/table-view.component';
 import { TableDisplayComponent } from './feature/table-view/components/table-display/table-display.component';
 import { ThemeToggleButtonComponent } from './core/static/components/theme-toggle-button/theme-toggle-button.component';
 import { ThemeService } from './core/services/theme.service';
-import { StatsViewComponent } from './feature/stats-view/stats-view.component';
 import { CommonModule } from '@angular/common';
 
 
@@ -14,7 +13,7 @@ import { CommonModule } from '@angular/common';
 @Component({
   standalone: true,
   imports: [RouterModule, NavbarComponent, GameViewComponent, TableViewComponent,
-    TableDisplayComponent, ThemeToggleButtonComponent, StatsViewComponent, CommonModule],
+    TableDisplayComponent, ThemeToggleButtonComponent, CommonModule],
   providers: [],
   selector: 'app-root',
   templateUrl: './app.component.html',
--- frontend/dig-kick-fe/src/app/app.routes.ts
+++ frontend/dig-kick-fe/src/app/app.routes.ts
@@ -2,18 +2,16 @@ import { Route, RouterModule } from '@angular/router';
 import { GameViewComponent } from './feature/game-view/game-view.component';
 import { ScoreViewComponent } from './feature/score-view/score-view.component';
 import { TableViewComponent } from './feature/table-view/table-view.component';
-import { StatsViewComponent } from './feature/stats-view/stats-view.component';
 import { NgModule } from '@angular/core';
 
 export const appRoutes: Route[] = [
-    {path: '', component: TableViewComponent},
-    {path: 'game/:tableId', component: GameViewComponent},
-    {path: 'score', component: ScoreViewComponent},
-    {path: 'stats', component: StatsViewComponent},
+    { path: '', component: TableViewComponent },
+    { path: 'game/:tableId', component: GameViewComponent },
+    { path: 'score', component: ScoreViewComponent },
 ];
 
 @NgModule({
     imports: [RouterModule.forRoot(appRoutes)],
     exports: [RouterModule]
-  })
-  export class AppRoutingModule { }
+})
+export class AppRoutingModule { }
