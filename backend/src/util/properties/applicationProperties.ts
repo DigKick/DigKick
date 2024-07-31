82c3bcfedc652df87c3538ca158257e573875a23
Maximilian König <maximilian.koenig@mein.gmx>
2024-07-31 17:19:29 +0200

    moved: property schema to own file

--- backend/src/util/properties/applicationProperties.ts
+++ backend/src/util/properties/applicationProperties.ts
@@ -2,43 +2,7 @@ import fs from "node:fs";
 import * as path from 'path';
 import {z} from "zod";
 import YAML from 'yaml'
-
-
-export const propertySchema = z.object({
-  digkick: z.object({
-    banner: z.boolean().default(true)
-  }).optional(),
-
-  mqtt: z.object({
-    login: z.object({
-      username: z.string().min(1),
-      password: z.string().min(1),
-    }),
-    host: z.string().min(1),
-  }),
-
-  db: z.object({
-    source: z.object({
-      database: z.object({
-        name: z.string().min(1),
-        suffix: z.string().min(1),
-      }),
-    })
-  }),
-
-  player: z.object({
-    name: z.object({
-      restrictions: z.object({
-        length: z.object({
-          min: z.number().positive({message: "Min player name length must be greater than 0!"}).optional().default(3),
-          max: z.number().positive({message: "Max player name length must be greater than 0!"}).optional().default(12)
-        }).optional(),
-      }).optional()
-    }).optional()
-  }).optional(),
-
-})
-
+import { propertySchema } from './propertySchema.ts';
 
 export class ApplicationProperties {
 
