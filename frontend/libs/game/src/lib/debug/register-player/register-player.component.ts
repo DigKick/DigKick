518d14672c8781af6339d3889d8d8622272c2b00
hiqs-chris <christian.bechtluft@hiqs.de>
2024-09-04 23:13:33 +0200

    Add Register-Player component

---
+++ frontend/libs/game/src/lib/debug/register-player/register-player.component.ts
@@ -0,0 +1,27 @@
+import { CommonModule } from '@angular/common';
+import { Component, input } from '@angular/core';
+import { DkMqttClientService } from '@dig-kick/services';
+import { TeamColor } from '@dig-kick/models';
+
+@Component({
+  selector: 'lib-game-debug-register-player',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './register-player.component.html',
+  styleUrl: './register-player.component.scss',
+})
+export class RegisterPlayerComponent {
+  tableId = input.required<string>();
+  teamColor = input<string>();
+
+  constructor(private _dkMqttClientService: DkMqttClientService) {}
+
+  registerPlayer() {
+    const randomId = self.crypto.randomUUID();
+    this._dkMqttClientService.registerPlayer(
+      randomId,
+      this.tableId(),
+      this.teamColor() === 'white' ? TeamColor.WHITE : TeamColor.BLACK,
+    );
+  }
+}
