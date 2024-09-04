501963bfd9b7b8cc2524b50e790361463f6dc84f
hiqs-chris <christian.bechtluft@hiqs.de>
2024-09-04 23:14:35 +0200

    Update game component

--- frontend/libs/game/src/lib/game/game.component.spec.ts
+++ frontend/libs/game/src/lib/game/game.component.spec.ts
@@ -1,9 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { GameComponent } from './game.component';
 import { TeamStubComponent } from '../team/team.component.stub';
-import { DkMqttClientService, GameService } from '@dig-kick/services';
-import { Game } from '@dig-kick/models';
-import { signal } from '@angular/core';
+import { DkMqttClientService } from '@dig-kick/services';
+import { TeamSwapStubComponent } from './team-swap/team-swap.component.stub';
+import { GameScoreStubComponent } from './game-score/game-score.component.stub';
+import { CommonModule } from '@angular/common';
+import { Environment, ENVIRONMENT } from '@dig-kick/models';
 
 describe('GameViewComponent', () => {
   let component: GameComponent;
@@ -11,31 +13,37 @@ describe('GameViewComponent', () => {
 
   const mqttClientMock: Partial<DkMqttClientService> = {
     doPublish: jest.fn(),
-  }
+  };
 
-  const gameServiceMock: Partial<GameService> = {
-    gameSignal: signal({
-      teamWhite: {
-        playerOne: {}
-      },
-      teamBlack: {
-        playerOne: {}
-      }
-    } as Game),
-    setId: jest.fn(),
-  }
+  const envMock: Environment = {
+    debug: false,
+    broker: {
+      hostname: '',
+      port: 80,
+      path: '',
+      protocol: 'ws',
+    },
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [GameComponent, TeamStubComponent],
-      providers: [{
-        provide: DkMqttClientService,
-        useValue: mqttClientMock
-      },
-  {
-    provide: GameService,
-    useValue: gameServiceMock
-  }]
+      imports: [
+        CommonModule,
+        GameComponent,
+        TeamStubComponent,
+        TeamSwapStubComponent,
+        GameScoreStubComponent,
+      ],
+      providers: [
+        {
+          provide: DkMqttClientService,
+          useValue: mqttClientMock,
+        },
+        {
+          provide: ENVIRONMENT,
+          useValue: envMock,
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(GameComponent);
--- frontend/libs/game/src/lib/game/game.component.ts
+++ frontend/libs/game/src/lib/game/game.component.ts
@@ -1,34 +1,52 @@
 import { CommonModule, NgOptimizedImage } from '@angular/common';
-import { Component, computed, inject, input } from '@angular/core';
+import {
+  Component,
+  computed,
+  inject,
+  input,
+  signal,
+  Signal,
+} from '@angular/core';
 import { DkMqttClientService } from '@dig-kick/services';
 import {
   Environment,
   ENVIRONMENT,
   Game,
-  ScoreChange,
+  Team,
   TeamColor,
 } from '@dig-kick/models';
 import { GameStore } from '@dig-kick/store';
 import { TeamComponent } from '../team/team.component';
+import { ScoreChangeComponent } from '../debug/score-change/score-change.component';
+import { TeamSwapComponent } from './team-swap/team-swap.component';
+import { GameScoreComponent } from './game-score/game-score.component';
+import { RegisterPlayerComponent } from '../debug/register-player/register-player.component';
 
 @Component({
   selector: 'lib-game-view',
   standalone: true,
-  imports: [CommonModule, TeamComponent, NgOptimizedImage],
+  imports: [
+    CommonModule,
+    TeamComponent,
+    NgOptimizedImage,
+    ScoreChangeComponent,
+    TeamSwapComponent,
+    GameScoreComponent,
+    RegisterPlayerComponent,
+  ],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.css',
+  styleUrl: './game.component.scss',
 })
 export class GameComponent {
-  whiteColor: string = TeamColor.WHITE;
-  blackColor: string = TeamColor.BLACK;
-
   tableId = input<string>();
+
+  private _isWhiteTeamFirst = signal(true);
 
   readonly environment: Environment = inject(ENVIRONMENT);
 
   readonly gameStore = inject(GameStore);
 
-  game = computed<Game | undefined>(() => {
+  public game = computed<Game | undefined>(() => {
     const tableId = this.tableId();
     if (tableId) {
       return this.gameStore.entityMap()[tableId];
@@ -36,69 +54,21 @@ export class GameComponent {
     return undefined;
   });
 
-  renameWhite = false;
-  renameBlack = false;
+  public firstTeam: Signal<Team | undefined> = computed(() => {
+    return this._isWhiteTeamFirst()
+      ? this.game()?.teamWhite
+      : this.game()?.teamBlack;
+  });
 
-    firstTeam: Signal<Team> = computed(() => {
-        if(this.isWhiteOnTheLeft()) {
-            return this.gameService.gameSignal().teamWhite
-        }
-        return this.gameService.gameSignal().teamBlack;
-    });
-
-    lastTeam: Signal<Team> = computed(() => {
-        if(this.isWhiteOnTheLeft()) {
-            return this.gameService.gameSignal().teamBlack
-        }
-        return this.gameService.gameSignal().teamWhite;
-    });
+  public lastTeam: Signal<Team | undefined> = computed(() => {
+    return this._isWhiteTeamFirst()
+      ? this.game()?.teamBlack
+      : this.game()?.teamWhite;
+  });
 
   constructor(private _dkMqttClientService: DkMqttClientService) {}
 
-  changeName(color: string) {
-    if (color === TeamColor.WHITE) {
-      this.renameWhite = true;
-    } else {
-      this.renameBlack = true;
-    }
-  }
-
-  submit(color: string, changeNameInputValue: string) {
-    let topic = '';
-    if (color === TeamColor.WHITE) {
-      const playerOneWhite = this.game()?.teamWhite?.playerOne;
-      if (playerOneWhite) {
-        playerOneWhite.name = changeNameInputValue;
-      }
-      topic = `table/${this.tableId()}/game/team/${color}/changename`;
-      this.renameWhite = false;
-    } else {
-      const playerOneBlack = this.game()?.teamBlack?.playerOne;
-      if (playerOneBlack) {
-        playerOneBlack.name = changeNameInputValue;
-      }
-      topic = `table/${this.tableId()}/game/team/${color}/changename`;
-      this.renameBlack = false;
-    }
-    this._dkMqttClientService.doPublish(
-      topic,
-      `{"newName": "${changeNameInputValue}"}`,
-    );
-  }
-
-  registerPlayer(game: Game, color: TeamColor, id: string) {
-    this._dkMqttClientService.registerPlayer(id, game.tableId, color);
-  }
-
   protected readonly TeamColor = TeamColor;
-  protected readonly ScoreChange = ScoreChange;
-
-  changeScore(game: Game, color: TeamColor, scoreChange: ScoreChange) {
-    const state = scoreChange === ScoreChange.INCREASE ? 1 : 2;
-    const topic = `table/${game.tableId}/game/team/${color}/button/${state}`;
-    this._dkMqttClientService.doPublish(topic, `{"pinOut": "HIGH"}`);
-    this._dkMqttClientService.doPublish(topic, `{"pinOut": "LOW"}`);
-  }
 
   reset(game: Game) {
     const topic = `table/${game.tableId}/game/team/white/button/0`;
@@ -107,6 +77,6 @@ export class GameComponent {
   }
 
   toggleDirection() {
-    this.isWhiteOnTheLeft.update((isOnTheLeft) => !isOnTheLeft);
+    this._isWhiteTeamFirst.update((isOnTheLeft) => !isOnTheLeft);
   }
 }
