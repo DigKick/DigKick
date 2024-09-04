17c9925c0742bc93569b6aa065a3c5d2aed5e1b8
Maximilian König <47715517+makoeta@users.noreply.github.com>
2024-09-04 22:37:34 +0200

    added: now changing player name in game obj when name is changed (#57)

--- backend/src/mqtt/player/events/playerEditEventMapper.ts
+++ backend/src/mqtt/player/events/playerEditEventMapper.ts
@@ -5,8 +5,13 @@ import type { PlayerNameEditPayload } from '../payloads/playerNameEditPayload.ts
 import { PlayerRepository } from '../../../database/modules/player/playerRepository.ts';
 import { PlayerDataPublisher } from '../publisher/playerDataPublisher.ts';
 import { GameEventType } from '../../game/events/gameEvent.ts';
+import { TopicTool } from '../../util/topicTool.ts';
+import { TeamColor } from '../../../models/team.ts';
+import { LoggerFactory } from '../../../logging/loggerFactory.ts';
 
 export class PlayerEditEventMapper implements EventMapper<PlayerEditEvent> {
+  logger = LoggerFactory.getLogger(PlayerEditEventMapper.name);
+
   constructor(private _playerEditHandler: PlayerEditHandler) {}
 
   map(event: PlayerEditEvent, topic: string, payload: PlayerNameEditPayload) {
@@ -14,6 +19,9 @@ export class PlayerEditEventMapper implements EventMapper<PlayerEditEvent> {
       case PlayerEditEvent.EDIT_NAME:
         if (!this._playerEditHandler.lastPlayerAdded) break;
         const playerToBeChanged = this._playerEditHandler.lastPlayerAdded;
+        const topicTool = new TopicTool(topic);
+        const teamColor = topicTool.getSegment(4).toLowerCase();
+
         this._playerEditHandler.lastPlayerAdded = undefined;
 
         PlayerRepository.updatePlayerName(
@@ -22,6 +30,24 @@ export class PlayerEditEventMapper implements EventMapper<PlayerEditEvent> {
         ).then(async () => {
           await PlayerDataPublisher.publishAll();
         });
+
+        const game = this._playerEditHandler.tableHandler.subject.game;
+
+        switch (teamColor) {
+          case TeamColor.WHITE:
+            if (game.teamWhite.playerOne) {
+              game.teamWhite.playerOne.name = payload.newName;
+            }
+            break;
+          case TeamColor.BLACK:
+            if (game.teamBlack.playerOne) {
+              game.teamBlack.playerOne.name = payload.newName;
+            }
+            break;
+          default:
+            this.logger.error('Invalid team color: ', teamColor);
+        }
+
         this._playerEditHandler.tableHandler.gameHandler.triggerEvent(
           GameEventType.PLAYER_CHANGE,
           topic,
