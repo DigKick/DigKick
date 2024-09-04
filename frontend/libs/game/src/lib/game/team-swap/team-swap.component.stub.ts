a06c1ad224c018cd4c59fa2eaf5b1846e4087873
hiqs-chris <christian.bechtluft@hiqs.de>
2024-09-04 21:52:29 +0200

    team-swap test & stub

---
+++ frontend/libs/game/src/lib/game/team-swap/team-swap.component.stub.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-game-team-swap',
+  standalone: true,
+  imports: [],
+  template: '',
+})
+export class TeamSwapStubComponent {}
