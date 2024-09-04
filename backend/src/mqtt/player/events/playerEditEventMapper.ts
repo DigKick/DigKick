b92bd300827e7f094d5bbbac561eedca58f10569
Maximilian König <maximilian.koenig@mein.gmx>
2024-09-04 22:31:39 +0200

    prettier

--- backend/src/mqtt/player/events/playerEditEventMapper.ts
+++ backend/src/mqtt/player/events/playerEditEventMapper.ts
@@ -1,28 +1,26 @@
-import type {EventMapper} from '../../global/eventMapper.ts';
-import {PlayerEditEvent} from './playerEditEvent.ts';
-import type {PlayerEditHandler} from '../handler/playerEditHandler.ts';
-import type {PlayerNameEditPayload} from '../payloads/playerNameEditPayload.ts';
-import {PlayerRepository} from '../../../database/modules/player/playerRepository.ts';
-import {PlayerDataPublisher} from '../publisher/playerDataPublisher.ts';
-import {GameEventType} from '../../game/events/gameEvent.ts';
-import {TopicTool} from "../../util/topicTool.ts";
-import {TeamColor} from "../../../models/team.ts";
-import {LoggerFactory} from "../../../logging/loggerFactory.ts";
+import type { EventMapper } from '../../global/eventMapper.ts';
+import { PlayerEditEvent } from './playerEditEvent.ts';
+import type { PlayerEditHandler } from '../handler/playerEditHandler.ts';
+import type { PlayerNameEditPayload } from '../payloads/playerNameEditPayload.ts';
+import { PlayerRepository } from '../../../database/modules/player/playerRepository.ts';
+import { PlayerDataPublisher } from '../publisher/playerDataPublisher.ts';
+import { GameEventType } from '../../game/events/gameEvent.ts';
+import { TopicTool } from '../../util/topicTool.ts';
+import { TeamColor } from '../../../models/team.ts';
+import { LoggerFactory } from '../../../logging/loggerFactory.ts';
 
 export class PlayerEditEventMapper implements EventMapper<PlayerEditEvent> {
+  logger = LoggerFactory.getLogger(PlayerEditEventMapper.name);
 
-  logger = LoggerFactory.getLogger(PlayerEditEventMapper.name)
-
-  constructor(private _playerEditHandler: PlayerEditHandler) {
-  }
+  constructor(private _playerEditHandler: PlayerEditHandler) {}
 
   map(event: PlayerEditEvent, topic: string, payload: PlayerNameEditPayload) {
     switch (event) {
       case PlayerEditEvent.EDIT_NAME:
         if (!this._playerEditHandler.lastPlayerAdded) break;
         const playerToBeChanged = this._playerEditHandler.lastPlayerAdded;
-        const topicTool = new TopicTool(topic)
-        const teamColor = topicTool.getSegment(4).toLowerCase()
+        const topicTool = new TopicTool(topic);
+        const teamColor = topicTool.getSegment(4).toLowerCase();
 
         this._playerEditHandler.lastPlayerAdded = undefined;
 
@@ -33,21 +31,21 @@ export class PlayerEditEventMapper implements EventMapper<PlayerEditEvent> {
           await PlayerDataPublisher.publishAll();
         });
 
-        const game = this._playerEditHandler.tableHandler.subject.game
+        const game = this._playerEditHandler.tableHandler.subject.game;
 
         switch (teamColor) {
           case TeamColor.WHITE:
             if (game.teamWhite.playerOne) {
               game.teamWhite.playerOne.name = payload.newName;
             }
-            break
+            break;
           case TeamColor.BLACK:
             if (game.teamBlack.playerOne) {
               game.teamBlack.playerOne.name = payload.newName;
             }
-            break
+            break;
           default:
-            this.logger.error("Invalid team color: ", teamColor)
+            this.logger.error('Invalid team color: ', teamColor);
         }
 
         this._playerEditHandler.tableHandler.gameHandler.triggerEvent(
