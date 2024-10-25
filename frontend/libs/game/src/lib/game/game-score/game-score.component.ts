315684b3ae9b3301b668de5ce3f30a1f566136fe
d3473r <d3473r@gmail.com>
2024-10-25 22:22:04 +0200

    unused import

--- frontend/libs/game/src/lib/game/game-score/game-score.component.ts
+++ frontend/libs/game/src/lib/game/game-score/game-score.component.ts
@@ -1,11 +1,10 @@
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Component, input } from '@angular/core';
-import { PlayerNameComponent } from '../../team/player-name/player-name.component';
 
 @Component({
   selector: 'lib-game-score',
   standalone: true,
-  imports: [CommonModule, NgOptimizedImage, PlayerNameComponent],
+  imports: [CommonModule, NgOptimizedImage],
   templateUrl: './game-score.component.html',
 })
 export class GameScoreComponent {
